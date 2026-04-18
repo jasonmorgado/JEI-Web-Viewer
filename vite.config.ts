3e7268461a2487f4f4e3b514e119307ff1c19b6e
Jason Morgado <jasonmorgado106@gmail.com>
2026-04-18 10:49:03 -0400

    GH Action to deploy site

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   // base must match repo name, because it's the URL path.
-  base: '/JEI-Web-Viewer',
+  base: '/JEI-Web-Viewer/',
   plugins: [vue()],
   resolve: {
     alias: {

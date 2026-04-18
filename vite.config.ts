3236aee38d2838793259184c4fccb425327587c4
Jason Morgado <jasonmorgado106@gmail.com>
2026-04-18 10:43:34 -0400

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base must match repo name, because it's the URL path.
+  base: '/JEI-Web-Viewer',
   plugins: [vue()],
   resolve: {
     alias: {
